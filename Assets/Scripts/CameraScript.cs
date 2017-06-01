ad36b2f9cb424d9f74e31f754c4039b191e5cf1c
Nekot94 <nekomato9@gmail.com>
2017-06-01 12:24:18 +0300

    f

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public float offsetZ;
+    [SerializeField] private GameObject cannon;
+
+    private void Start ()
+    {
+        offsetZ = cannon.transform.localScale.z * 15;
+        LookAtCannon();
+	}
+	
+
+    private void LookAtCannon()
+    {
+        Vector3 temp = transform.position;
+        temp.z = cannon.transform.position.x - offsetZ;
+        transform.position = temp;
+    }
+}
---
+++ Assets/Scripts/CannonScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CannonScript : MonoBehaviour {
+
+    [SerializeField] private Rigidbody bullet;
+    [SerializeField] private Transform fireTransform;
+    [SerializeField] private GameObject gun;
+    [SerializeField] private Text angleText;
+    [SerializeField] private Text forceText;
+
+
+
+    private bool fired;
+    private float nextFireTime;
+    private float launchForce;
+
+
+
+
+    public void SetAngle(float angle)
+    {
+        gun.transform.localEulerAngles = new Vector3(angle, 0, 0);
+        angleText.text = "Угол: " + angle.ToString();
+    }
+
+    public void SetForce(float force)
+    {
+        launchForce = force;
+        forceText.text = "Сила: " + force.ToString();
+    }
+
+    public void Fire(float fireRate)
+    {
+        if (Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            fired = true;
+
+            Rigidbody shellInstance =
+                Instantiate(bullet, fireTransform.position, fireTransform.rotation) as Rigidbody;
+
+            shellInstance.velocity = launchForce * fireTransform.forward;
+
+            //m_ShootingAudio.clip = m_FireClip;
+            //m_ShootingAudio.Play();
+
+        }
+
+    }
+}
